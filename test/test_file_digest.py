42f6c34976e29c314805c60751b4852d8fbb7b48
Martin Liska <mliska@suse.cz>
2020-12-03 10:34:19 +0100

    Add tests for File Digest check.

--- test/test_file_digest.py
+++ test/test_file_digest.py
@@ -13,9 +13,29 @@ def digestcheck():
     return output, test
 
 
-@pytest.mark.parametrize('package', ['binary/file-signature'])
+@pytest.mark.parametrize('package', ['binary/file-signature-good'])
 def test_signatures(tmpdir, package, digestcheck):
     output, test = digestcheck
     test.check(get_tested_package(package, tmpdir))
     out = output.print_results(output.results)
     assert not out
+
+
+@pytest.mark.parametrize('package', ['binary/file-signature-symlinks'])
+def test_signatures_symlinks(tmpdir, package, digestcheck):
+    output, test = digestcheck
+    test.check(get_tested_package(package, tmpdir))
+    out = output.print_results(output.results)
+    assert len(output.results) == 2
+    assert 'file-signature-symlinks.x86_64: E: cron-file-symlink /etc/cron.daily/symlink' in out
+    assert 'file-signature-symlinks.x86_64: E: cron-file-digest-ghost /etc/cron.daily/test-me-ghost' in out
+
+
+@pytest.mark.parametrize('package', ['binary/file-signature-bad'])
+def test_signatures_bad(tmpdir, package, digestcheck):
+    output, test = digestcheck
+    test.check(get_tested_package(package, tmpdir))
+    out = output.print_results(output.results)
+    assert len(output.results) == 2
+    assert 'file-signature-bad.x86_64: E: cron-file-digest-unauthorized /etc/cron.daily/suse.de-sarg' in out
+    assert 'file-signature-bad.x86_64: E: cron-file-digest-unauthorized /etc/cron.daily/test-me' in out
