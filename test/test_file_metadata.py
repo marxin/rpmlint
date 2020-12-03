5df3699db0b213ceb61e5ad8ff02e11c52445f4b
Martin Liska <mliska@suse.cz>
2020-12-03 21:10:01 +0100

    Remove on ambiguous test from file metadata check.

--- test/test_file_metadata.py
+++ test/test_file_metadata.py
@@ -18,9 +18,7 @@ def test_signatures(tmpdir, package, metadatacheck):
     output, test = metadatacheck
     test.check(get_tested_package(package, tmpdir))
     out = output.print_results(output.results)
-    assert len(output.results) == 5
-    assert 'E: permissions-incorrect-owner /etc/cron.daily/test-me expected:root has:marxin' in out
     assert 'E: permissions-incorrect-mode /etc/cron.daily/test-me expected:crw-rw---- has:-rw-r--r--' in out
-    assert 'E: permissions-incorrect-group /etc/cron.daily/test-me expected:tty has:users' in out
-    assert 'E: permissions-incorrect-device-minor /etc/cron.daily/test-me expected:12 has:46' in out
-    assert 'E: permissions-incorrect-device-major /etc/cron.daily/test-me expected:55 has:0' in out
+    assert 'E: permissions-incorrect-group /etc/cron.daily/test-me expected:tty has:' in out
+    assert 'E: permissions-incorrect-device-minor /etc/cron.daily/test-me expected:12 has:' in out
+    assert 'E: permissions-incorrect-device-major /etc/cron.daily/test-me expected:55 has:' in out
